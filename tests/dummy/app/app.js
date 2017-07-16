621a6349aa54b87e7edd1252363da790c5602e77
kumkanillam <kumkanillam@gmail.com>
2017-07-17 00:37:42 +0530

    removed factory injection deprecation error.

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -5,12 +5,11 @@ import config from './config/environment';
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
+    modulePrefix: config.modulePrefix,
+    podModulePrefix: config.podModulePrefix,
+    Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
